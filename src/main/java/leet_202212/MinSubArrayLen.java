f15ec043040232b85bbfb5321b9adf9a9dada54e
wanyinsheng <wan_yinsheng@163.com>
2022-12-11 15:54:50 +0800

    20221211

---
+++ src/main/java/leet_202212/MinSubArrayLen.java
@@ -0,0 +1,22 @@
+package leet_202212;
+
+public class MinSubArrayLen {
+	public int minSubArrayLen(int target, int[] nums) {
+		int n=nums.length;
+		if(n==0)
+			return 0;
+		int ans=Integer.MAX_VALUE;
+		int start=0,end=0;
+		int sum=0;
+		while(end<n){//开始一个窗口
+			sum+=nums[end];
+			while(sum>=target) {//移动一个窗口
+				ans=Math.min(ans,end-start+1);
+				sum-=nums[start];
+				start++;
+			}
+			end++;
+		}
+		return ans == Integer.MAX_VALUE ? 0 : ans;
+	}
+}
