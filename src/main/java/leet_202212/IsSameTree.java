95238e1337d8f280e5e07ba775479e70e4fb274d
wanyinsheng <wan_yinsheng@163.com>
2022-12-16 22:55:10 +0800

    20221216

---
+++ src/main/java/leet_202212/IsSameTree.java
@@ -0,0 +1,22 @@
+package leet_202212;
+
+import leet_202203.TreeNode;
+
+/**
+ * 100. 相同的树
+ */
+public class IsSameTree {
+	public boolean isSameTree(TreeNode p, TreeNode q) {
+		if (p == null && q == null) {
+			return true;
+		} else if (p == null || q == null) {
+			return false;
+		} else if (p.val != q.val) {
+			return false;
+		} else {
+			return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+		}
+
+
+	}
+}
