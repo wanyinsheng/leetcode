a2e1f3d5764877f60be9a1bb2d6f3a1d492d6c77
wanyinsheng <wan_yinsheng@163.com>
2022-12-16 23:03:12 +0800

    20221216

--- src/main/java/leet_202212/IsValidBST.java
+++ src/main/java/leet_202212/IsValidBST.java
@@ -2,6 +2,9 @@ package leet_202212;
 
 import leet_202203.TreeNode;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  * 98. 验证二叉搜索树
  */
@@ -29,5 +32,32 @@ public class IsValidBST {
 		//左右节点子树的根节点是否在范围内
 		return isValidBST(node.left, lower, node.val) && isValidBST(node.right, node.val, upper);
 	}
+
+	/**
+	 * 使用中序遍历
+	 * @param root
+	 * @return
+	 */
+	public boolean isValidBST1(TreeNode root) {
+		Deque<TreeNode> stack = new LinkedList<TreeNode>();
+		double inorder = -Double.MAX_VALUE;//维护一个上一个中序遍历的节点值
+
+		while (!stack.isEmpty() || root != null) {
+			//先遍历到最左侧的叶子节点
+			while (root != null) {
+				stack.push(root);
+				root = root.left;
+			}
+			root = stack.pop();
+			// 如果中序遍历得到的节点的值小于等于前一个 inorder，说明不是二叉搜索树
+			if (root.val <= inorder) {
+				return false;
+			}
+			inorder = root.val;
+			root = root.right;
+		}
+		return true;
+	}
+
 
 }
