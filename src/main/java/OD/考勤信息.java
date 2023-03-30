cd7f4b5c0a37664fb0795a4afe0013875530698e
wanyinsheng <wan_yinsheng@163.com>
2023-03-30 16:21:38 +0800

    20230330

--- src/main/java/OD/考勤信息.java
+++ src/main/java/OD/考勤信息.java
@@ -1,5 +1,10 @@
 package OD;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Stack;
+
 /**
  * @Author wanys
  * @Date 2023/3/30 11:18
@@ -7,10 +12,46 @@ package OD;
  **/
 public class 考勤信息 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        System.out.println(method(line));
 
     }
-    static boolean method(String str){
 
+    static boolean method(String str) {
+        Stack<String> stack = new Stack<>();
+        Queue<String> queue = new LinkedList<>();
+        String[] strs = str.split(" ");
+        int absent = 0;
+        int myCount = 0;
+        for (int i = 0; i < strs.length; i++) {
+            String cur = strs[i];
+            if (cur.equals("absent")) {
+                if (++absent > 1) {
+                    return false;
+                }
+            }
+            //用栈判断是否有连续早退或者迟到
+            if (cur.equals("late") || cur.equals("leaveearly")) {
+                if (stack.peek().equals("late") || stack.peek().equals("leaveearly")) {
+                    return false;
+                } else {
+                    myCount++;
+                }
+            }
+            stack.push(cur);
+            queue.offer(cur);
+            //判断三次以上迟到或早退
+            if (queue.size() == 7 && myCount > 3) {
+                return false;
+            } else {
+                String poll = queue.poll();
+                if (poll.equals("late") || poll.equals("leaveearly")) {
+                    myCount--;
+                }
+            }
+
+        }
         return true;
 
     }
