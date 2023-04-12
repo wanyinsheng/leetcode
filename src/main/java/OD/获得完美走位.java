4b5ca2629c333fa44a0ff5815ba12f08815be459
wanyinsheng <wan_yinsheng@163.com>
2023-04-12 17:15:30 +0800

    20230412

--- src/main/java/OD/获得完美走位.java
+++ src/main/java/OD/获得完美走位.java
@@ -1,6 +1,7 @@
 package OD;
 
 import java.util.HashMap;
+import java.util.Scanner;
 
 /**
  * @Author wanys
@@ -9,21 +10,28 @@ import java.util.HashMap;
  **/
 public class 获得完美走位 {
     public static void main(String[] args) {
-        //如果不是4的倍数  输出0
-        String str="";
-        if(str.length()%4!=0){
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        //输入长度是否符合
+        if (null == str || str.length() < 1 || str.length() > 100000) {
             System.out.println("0");
+            return;
+        }
+        //如果不是4的倍数  输出0
+        if (str.length() % 4 != 0) {
+            System.out.println("0");
+            return;
         }
         char[] chars = str.toCharArray();
-        int length=chars.length;
-        int number=length/4;
+        int length = chars.length;
+        int number = length / 4;
         HashMap<Character, Integer> map = new HashMap<>();
         //字符串中只能有  ASWD
-        for(char ch:chars){
-            if(ch!='A'&&ch!='S'&&ch!='D'&&ch!='W'){
+        for (char ch : chars) {
+            if (ch != 'A' && ch != 'S' && ch != 'D' && ch != 'W') {
                 System.out.println("0");
             }
-            map.put(ch,map.getOrDefault(ch,0)+1);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
             //计算大于平均值的数 和小于平均值的数
             //答案就是大于平均值的数或者小于平均值的数
 
