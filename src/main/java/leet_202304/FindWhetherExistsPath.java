dd102a76280cff7578366fd53f75726ff3755052
wanyinsheng <wan_yinsheng@163.com>
2023-04-25 15:46:40 +0800

    20230425

---
+++ src/main/java/leet_202304/FindWhetherExistsPath.java
@@ -0,0 +1,16 @@
+package leet_202304;
+
+/**
+ * @Author wanys
+ * @Date 2023/4/25 15:08
+ * @Version 1.0
+ **/
+public class FindWhetherExistsPath {
+    public boolean findWhetherExistsPath(int n, int[][] graph, int start, int target) {
+
+        return false;
+    }
+    public boolean dfs(){
+        return false;
+    }
+}
